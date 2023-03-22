60ca8060c3d353122b5de49a943a3e6d473df679
kye-li <75037993+kye-li@users.noreply.github.com>
2023-03-22 22:29:56 +0000

    updated proxy

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(App) {
     App.use(
         '/api',
         createProxyMiddleware({
-            target: 'https://f4b3-82-132-217-127.eu.ngrok.io/',
+            target: 'http://127.0.0.1:5049',
             changeOrigin: true,
         })
     );
