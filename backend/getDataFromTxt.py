232d70eeb66b63cfd5f8ee5c49b3ad77f5b0a182
kye-li <75037993+kye-li@users.noreply.github.com>
2023-03-27 05:14:27 +0100

    updated references

--- backend/getDataFromTxt.py
+++ backend/getDataFromTxt.py
@@ -14,28 +14,28 @@ directory3 = r"C:\Users\kye\Documents\Twitter FYP\extra pre-election tweets"
 
 
 # ref: https://docs.python.org/3/library/csv.html
-# the open() and DictWriter methods used in code below were written while learning from the Python documentation as referenced
+# the open() and DictWriter methods used in code below were re-used code from the Python documentation as referenced
 
 def write_to_csv(data):
     # use 'a' to continue appending rows to original file, 'w' will clear file
     # check first if file is empty, if it is, write header
-    with open('data/tweets.csv', 'a', newline='', encoding='utf8') as csvfile:
-        fieldnames = ['edit_history_tweet_ids', 'geo', 'created_at', 'text', 'id']
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames, extrasaction='ignore')
+    with open('data/tweets.csv', 'a', newline='', encoding='utf8') as csvfile:      # taken from reference
+        fieldnames = ['edit_history_tweet_ids', 'geo', 'created_at', 'text', 'id']    # taken from reference
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames, extrasaction='ignore')    # taken from reference
 
         # check if file is empty, write header
         # reference: https://thispointer.com/python-three-ways-to-check-if-a-file-is-empty/
-        if os.stat('data/tweets.csv').st_size == 0: # this line of code taken from reference
-            writer.writeheader()
+        if os.stat('data/tweets.csv').st_size == 0:    # this line of code taken from reference
+            writer.writeheader()   # this line of code taken from reference (csv.html link above)
             print('file is empty')
         for tweet in data:
             # if Dict has key 'geo', writerow
             # if Dict does not have key 'geo', add new key:value pair, insert an '' empty string
             if 'geo' in tweet:
-                writer.writerow(tweet)
+                writer.writerow(tweet)   # this line of code taken from reference (csv.html link above)
             else:
                 tweet['geo'] = ''
-                writer.writerow(tweet)
+                writer.writerow(tweet)   # this line of code taken from reference (csv.html link above)
 
 # ref: https://docs.python.org/3/library/os.html
 # the os.listdir() method used in code below was written while learning from the Python documentation as referenced
@@ -48,9 +48,9 @@ def read_files(directory):
     # load response as python dict
     # write to csv
 
-    for file in os.listdir(directory):
+    for file in os.listdir(directory):    # this line of code taken from reference
         # https://www.w3schools.com/python/ref_string_format.asp
-        filepath = r"{directory}\{file}".format(directory=directory, file=file)
+        filepath = r"{directory}\{file}".format(directory=directory, file=file)    # this line of code taken from reference
         print(filepath)
         f = open(filepath, "r", encoding="utf8")
         response = f.read()
