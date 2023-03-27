cfce098cc186bb59d023a68497f9e213f0269025
kye-li <75037993+kye-li@users.noreply.github.com>
2023-03-27 05:55:52 +0100

    updated references

--- frontend/src/components/LineChart.js
+++ frontend/src/components/LineChart.js
@@ -1,4 +1,4 @@
-// ref: https://www.chartjs.org/docs/latest/charts/line.html
+// all code below were written while learning and referring to documentation: https://www.chartjs.org/docs/latest/charts/line.html
 import React from 'react';
 import {
     Chart as ChartJS,
@@ -47,12 +47,6 @@ const options = {
                 borderColor: 'rgb(255, 99, 132)',
                 backgroundColor: 'rgba(255, 99, 132, 0.5)'
             }
-            // {
-            //     label: 'Dataset 2',
-            //     data: [['Jan',700], ['Feb',600], ['Mar',500], ['Apr',400], ['May',300], ['Jun',200], ['Jul',100]],
-            //     borderColor: 'rgb(53, 162, 235)',
-            //     backgroundColor: 'rgba(53, 162, 235, 0.5)',
-            // },
         ],
     };
 
